fc005993881fa4a36834447135c8ba30a57ce2bf
Eduardoo1422 <Eduardosampaiopablo@gmail.com>
2026-09-20 17:06:51 -0300

    feat: add admin dashboard and authentication

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import { keyRoutes } from './key.routes';
 import { clientRoutes } from './client.routes';
 import { resourceRoutes } from './resource.routes';
+import { adminRoutes } from './admin.routes';
 
 const routes = Router();
 
+routes.use('/api/admin', adminRoutes);
 routes.use('/api/admin/keys', keyRoutes);
 routes.use('/api/admin/resources', resourceRoutes);
 routes.use('/api/client', clientRoutes);
